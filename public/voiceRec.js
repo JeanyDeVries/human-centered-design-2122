538f15bb0eee7e2ae98c3b2a18a933291cb2fcb5
Jeany de Vries <jeanydevries@hotmail.com>
2022-04-20 15:31:49 +0200

    eventlistener to check which data is selected

--- public/voiceRec.js
+++ public/voiceRec.js
@@ -10,6 +10,11 @@ else{
     console.log("browser does not support the speech recognition")
 }
 
+document.addEventListener('selectionchange', () => {
+    var selObj = window.getSelection();
+    var selectedText = selObj.toString();
+});
+
 recognition.addEventListener('result', (message) => {
     const text = Array.from(message.results)
         .map(result => result[0])
